1732f2272595754a07b3ce95d8f10f07ca95a634
sbx_user1051 <sbx_user1051@169.254.104.189>
2024-10-01 20:02:47 +0000

    Tue Oct 01 20:02:47 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,12 +1,20 @@
-
+from collections import defaultdict
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
     sku_list = skus.split(",")
 
+    total_skus = defaultdict(int)
+
     for sku in sku_list:
         if sku not in "ABCD":
             return -1
+        total_skus[sku] += 1
+
+    total_cost = 0
+
+    total_skus[]
+
 
 
