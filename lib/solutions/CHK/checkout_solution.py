7a24b93d1e6f5bca03b01b86d8bc3fc75bd858d2
sbx_user1051 <sbx_user1051@169.254.104.189>
2024-10-01 20:49:01 +0000

    Tue Oct 01 20:49:01 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,30 +1,86 @@
 from collections import defaultdict
 import attrs
-
+from typing import Protocol, List
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-class MultiBuy:
+@attrs.define()
+class Price:
+    quantity: int
+    price: int
 
 
-def cost_a(count: int) -> int:
-    total_cost = 200 * (count // 5)
-    total_cost += 130 * ((count % 5) // 3)
-    total_cost += 50 * ((count % 5) % 3)
-    return total_cost
+class BuyOffer(Protocol):
+    
+    def calculate_cost(self, quantity: int) -> int:
+        pass
 
-def cost_b(count: int, count_e: int) -> int:
-    # remove 1 count per 2 E
-    count -= count_e // 2
-    if count < 0:
-        return 0
-    total_cost = 45 * (count // 2)
-    total_cost += 30 * ( count % 2)
-    return total_cost
+@attrs.define()
+class SingleBuyOffer:
+    single_price: int
+    
+    def calculate_cost(self, quantity: int) -> int:
+        return self.single_price * quantity
 
-def cost_f(count: int) -> int:
-    total_cost = 10 * (count - (count // 3))
-    return total_cost
+@attrs.define()
+class DoubleBuyOffer:
+    single_price: int
+    double_price: int
+    
+    def calculate_cost(self, quantity: int) -> int:
+        cost = self.double_price * (count // 2)
+        cost += self.single_price * ( count % 2)
+        return cost
+
+
+@attrs.define()
+class TripleBuyOffer:
+    single_price: int
+    double_price: int
+    triple_price: int
+    
+
+    def calculate_cost(self, quantity: int) -> int:
+        cost = self.triple_price * (quantity // 5)
+        cost += self.double_price * ((quantity % 5) // 3)
+        cost += self.single_price * ((quantity % 5) % 3)
+        return cost
+
+@attrs.define()
+class MultiBuyOffer:
+    prices: List[Price]
+
+    def calculate_cost(self, quantity: int) -> int:
+        cost = 0
+        prices_sorted = self.prices.sort(key=lambda x: x.quantity, reverse=True)
+        print(self.prices[0].quantity)
+        for price in prices_sorted:
+            quantity_at_price = quantity // price.quantity
+            cost += quantity_at_price * price.price
+            quantity -= quantity_at_price
+        return cost
+
+class OfferRegistry:
+    pass
+
+# def cost_a(quantity: int) -> int:
+#     total_cost = 200 * (count // 5)
+#     total_cost += 130 * ((count % 5) // 3)
+#     total_cost += 50 * ((count % 5) % 3)
+#     return total_cost
+#
+# def cost_b(quantity: int, count_e: int) -> int:
+#     # remove 1 count per 2 E
+#     count -= count_e // 2
+#     if count < 0:
+#         return 0
+#     total_cost = 45 * (count // 2)
+#     total_cost += 30 * ( count % 2)
+#     return total_cost
+#
+# def cost_f(quantity: int) -> int:
+#     total_cost = 10 * (count - (count // 3))
+#     return total_cost
 
 def checkout(skus: str) -> int:
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,29 +1,35 @@
 from solutions.CHK import checkout_solution
 
-def test_sum_a():
-    assert 450 == checkout_solution.cost_a(11)
-    assert 500 == checkout_solution.cost_a(12)
-    assert 380 == checkout_solution.cost_a(9)
-    assert 0 == checkout_solution.cost_a(0)
+# def test_sum_a():
+#     assert 450 == checkout_solution.cost_a(11)
+#     assert 500 == checkout_solution.cost_a(12)
+#     assert 380 == checkout_solution.cost_a(9)
+#     assert 0 == checkout_solution.cost_a(0)
+#
+# def test_sum_b():
+#     assert 45 == checkout_solution.cost_b(2, 0)
+#     assert 75 == checkout_solution.cost_b(3, 0)
+#     assert 75 == checkout_solution.cost_b(3, 1)
+#     assert 45 == checkout_solution.cost_b(3, 2)
+#     assert 45 == checkout_solution.cost_b(3, 3)
+#     assert 0 == checkout_solution.cost_b(0, 2)
+#     assert 0 == checkout_solution.cost_b(0, 0)
+#
+# def test_sum_f():
+#     assert 10 == checkout_solution.cost_f(1)
+#     assert 20 == checkout_solution.cost_f(2)
+#     assert 20 == checkout_solution.cost_f(3)
+#     assert 30 == checkout_solution.cost_f(4)
+#     assert 80 == checkout_solution.cost_f(11)
+#     assert 0 == checkout_solution.cost_f(0)
+#
+# def test_checkout():
+#     result = checkout_solution.checkout("BAAAABBCDEEFFF")
+#     expected = 360
+#     assert result == expected
+#
+#
+class TestMultiBuyOffer:
 
-def test_sum_b():
-    assert 45 == checkout_solution.cost_b(2, 0)
-    assert 75 == checkout_solution.cost_b(3, 0)
-    assert 75 == checkout_solution.cost_b(3, 1)
-    assert 45 == checkout_solution.cost_b(3, 2)
-    assert 45 == checkout_solution.cost_b(3, 3)
-    assert 0 == checkout_solution.cost_b(0, 2)
-    assert 0 == checkout_solution.cost_b(0, 0)
-
-def test_sum_f():
-    assert 10 == checkout_solution.cost_f(1)
-    assert 20 == checkout_solution.cost_f(2)
-    assert 20 == checkout_solution.cost_f(3)
-    assert 30 == checkout_solution.cost_f(4)
-    assert 80 == checkout_solution.cost_f(11)
-    assert 0 == checkout_solution.cost_f(0)
-
-def test_checkout():
-    result = checkout_solution.checkout("BAAAABBCDEEFFF")
-    expected = 360
-    assert result == expected
+    def test_calculate_cost(self):
+        assert 300 == checkout_solution.MultiBuyOffer([checkout_solution.Price(1,100)]).calculate_cost(3)
