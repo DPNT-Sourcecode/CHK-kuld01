4a6cb0ce54e53f4fa8530fa99828071d8455bf15
sbx_user1051 <sbx_user1051@169.254.104.189>
2024-10-01 20:13:45 +0000

    Tue Oct 01 20:13:45 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,5 +1,14 @@
 from solutions.CHK import checkout_solution
 
+def test_sum_a():
+    assert 180 == checkout_solution.cost_a(4)
+    assert 130 == checkout_solution.cost_a(3)
+    assert 0 == checkout_solution.cost_a(0)
+
+def test_sum_b():
+    assert 45 == checkout_solution.cost_b(2)
+    assert 75 == checkout_solution.cost_b(3)
+    assert 0 == checkout_solution.cost_b(0)
 
 def test_checkout():
     result = checkout_solution.checkout("B,A,A,A,A,B,B,C,D")
