45536469b4d99bca2a302d4a134dbe772be10b2e
sbx_user1051 <sbx_user1051@169.254.104.189>
2024-10-01 20:22:31 +0000

    Tue Oct 01 20:22:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,12 +4,14 @@ from collections import defaultdict
 # skus = unicode string
 
 def cost_a(count: int) -> int:
-    total_cost = 150 * (count // 5)
+    total_cost = 200 * (count // 5)
     total_cost += 130 * ((count % 5) // 3)
-    total_cost += 130 * ((count % 5) % 3)
+    total_cost += 50 * ((count % 5) % 3)
     return total_cost
 
-def cost_b(count: int) -> int:
+def cost_b(count: int, count_e: int) -> int:
+    # remove 1 count per 2 E
+    count -= count_e // 2
     total_cost = 45 * (count // 2)
     total_cost += 30 * ( count % 2)
     return total_cost
@@ -19,7 +21,7 @@ def checkout(skus: str) -> int:
     total_skus = defaultdict(int)
 
     for sku in skus:
-        if sku not in "ABCD":
+        if sku not in "ABCDE":
             return -1
         total_skus[sku] += 1
 
@@ -29,13 +31,16 @@ def checkout(skus: str) -> int:
     total_cost += cost_a(total_skus["A"])
 
     # B cost
-    total_cost += cost_b(total_skus["B"])
+    total_cost += cost_b(total_skus["B"], total_skus["E"])
 
     # C cost
     total_cost += 20 * total_skus["C"]
 
     # D cost
     total_cost += 15 * total_skus["D"]
+
+    # E cost
+    total_cost += 40 * total_skus["E"]
 
     return total_cost
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,16 +1,20 @@
 from solutions.CHK import checkout_solution
 
 def test_sum_a():
-    assert 180 == checkout_solution.cost_a(4)
-    assert 130 == checkout_solution.cost_a(3)
+    assert 450 == checkout_solution.cost_a(11)
+    assert 500 == checkout_solution.cost_a(12)
+    assert 380 == checkout_solution.cost_a(9)
     assert 0 == checkout_solution.cost_a(0)
 
 def test_sum_b():
-    assert 45 == checkout_solution.cost_b(2)
-    assert 75 == checkout_solution.cost_b(3)
-    assert 0 == checkout_solution.cost_b(0)
+    assert 45 == checkout_solution.cost_b(2, 0)
+    assert 75 == checkout_solution.cost_b(3, 0)
+    assert 75 == checkout_solution.cost_b(3, 1)
+    assert 45 == checkout_solution.cost_b(3, 2)
+    assert 45 == checkout_solution.cost_b(3, 3)
+    assert 0 == checkout_solution.cost_b(0, 0)
 
 def test_checkout():
-    result = checkout_solution.checkout("BAAAABBCD")
-    expected = 290
+    result = checkout_solution.checkout("BAAAABBCDEE")
+    expected = 340
     assert result == expected
